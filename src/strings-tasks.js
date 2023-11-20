461326a9902018a79610b17fd9da290ce606a525
ivi0540 <ivi0540@gmail.com>
2023-11-20 12:18:35 +0300

    feat: solved repeatString task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -133,8 +133,11 @@ function removeTrailingWhitespaces(/* value */) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(value, count) {
+  if (count <= 0) {
+    return '';
+  }
+  return value.repeat(count);
 }
 
 /**
